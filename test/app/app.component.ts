646430ca057eb82c0b6dd86c019edccc11e4d8e2
Ara Winters <ara.winters@gmail.com>
2019-07-23 20:43:21 -0700

    add ability to toggle matchkeys/link labels. #6

--- test/app/app.component.ts
+++ test/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sdk-graph-components';
+  @Input() public showGraphMatchKeys = false;
+
+  public toggleGraphMatchKeys(event): void {
+    let _checked = false;
+    if (event.target) {
+      _checked = event.target.checked;
+    } else if (event.srcElement) {
+      _checked = event.srcElement.checked;
+    }
+    // console.warn('toggleGraphMatchKeys: ', _checked);
+    this.showGraphMatchKeys = _checked;
+  }
 
   public onContextMenuClick(event: any) {
     console.log('Context Menu Click for: ', event);
